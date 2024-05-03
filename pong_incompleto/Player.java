2438179981e83a14344f1e9cf18721ce920b74c9
Joao Pedro <joaopopm@usp.br>
2024-05-03 15:38:58 -0300

    criei a classe Player que esta funcionando

--- pong_incompleto/Player.java
+++ pong_incompleto/Player.java
@@ -7,6 +7,16 @@ import java.awt.*;
 
 public class Player {
 
+	private double cx;
+	private double cy;
+	private double width;
+	private double height;
+	private Color color;
+	private String id;
+	private double[] v_limit;  
+	private double speed;
+
+
 	/**
 		Construtor da classe Player.
 
@@ -21,7 +31,14 @@ public class Player {
 	*/
 
 	public Player(double cx, double cy, double width, double height, Color color, String id, double [] v_limit, double speed){
-	
+		this.cx = cx;
+		this.cy = cy;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+		this.id = id;
+		this.v_limit = v_limit;  
+		this.speed = speed;
 	}
 
 	/**
@@ -31,7 +48,7 @@ public class Player {
 	public void draw(){
 
 		GameLib.setColor(Color.GREEN);
-		GameLib.fillRect(80, 300, 20, 100);
+		GameLib.fillRect(cx, cy, width, height);
 	}
 
 	/**
@@ -43,7 +60,7 @@ public class Player {
 	*/
 
 	public void moveUp(long delta){
-
+		this.cy -= this.speed;
 	}
 
 	/**
@@ -55,7 +72,7 @@ public class Player {
 	*/
 
 	public void moveDown(long delta){
-
+		this.cy += this.speed;
 	}
 
 	/**
@@ -64,8 +81,7 @@ public class Player {
 	*/
 
 	public String getId() { 
-
-		return ""; 
+		return this.id; 
 	}
 
 	/**
@@ -74,8 +90,7 @@ public class Player {
 	*/
 
 	public double getWidth() { 
-
-		return 20; 
+		return this.width; 
 	}
 
 	/**
@@ -84,8 +99,7 @@ public class Player {
 	*/
 
 	public double getHeight() { 
-
-		return 100;
+		return this.height;
 	}
 
 	/**
@@ -94,8 +108,7 @@ public class Player {
 	*/
 
 	public double getCx() { 
-		
-		return 80;
+		return this.cx;
 	}
 
 	/**
@@ -104,8 +117,7 @@ public class Player {
 	*/
 
 	public double getCy() { 
-	
-		return 300;
+		return this.cy;
 	}
 }
 
